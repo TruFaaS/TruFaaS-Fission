073214aec04e67e8e4187f66b4469db35345ba55
Avishka-Shamendra <avishka199849@gmail.com>
2023-04-29 11:40:36 +0530

    Changed external comp url to localhost

--- trufaas/config.go
+++ trufaas/config.go
@@ -2,7 +2,8 @@ package trufaas
 
 const (
 	//ExternalCompBaseURL = "https://trufaas-external.onrender.com"
-	ExternalCompBaseURL = "https://turfaas-external-comp-avishka.onrender.com"
+	//ExternalCompBaseURL = "https://turfaas-external-comp-avishka.onrender.com"
+	ExternalCompBaseURL = "http://localhost:8080"
 
 	CreateURL = ExternalCompBaseURL + "/fn/create"
 	VerifyURL = ExternalCompBaseURL + "/fn/verify"
