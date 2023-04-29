02478edb6f46cbf60c8cfde8695e7e6dc9fb43a1
Avishka-Shamendra <avishka199849@gmail.com>
2023-04-30 01:10:03 +0530

    Changed external comp url

--- trufaas/config.go
+++ trufaas/config.go
@@ -1,9 +1,24 @@
 package trufaas
 
 const (
-	//ExternalCompBaseURL = "https://trufaas-external.onrender.com"
+	ExternalCompBaseURL = "https://trufaas-external.onrender.com"
 	//ExternalCompBaseURL = "https://turfaas-external-comp-avishka.onrender.com"
-	ExternalCompBaseURL = "http://localhost:8080"
+
+	/*
+		Get your https url for external component running on local host
+
+			Method 1:
+				Go to https://theboroer.github.io/localtunnel-www/ and install local tunnel
+				Then run TruFaaS external component locally
+				Get https url by running 'lt --port 8080'
+
+			Method 2:
+				Go to https://ngrok.com and setup ngrok with auth token
+				Then run TruFaaS external component locally
+				Get https url by running 'ngrok http 8080'
+	*/
+
+	//ExternalCompBaseURL = "{URL}"
 
 	CreateURL = ExternalCompBaseURL + "/fn/create"
 	VerifyURL = ExternalCompBaseURL + "/fn/verify"
